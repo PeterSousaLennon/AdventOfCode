d8a8aaed93e9ad3a55468e221228d04433a09963
PeterSousaLennon <peterlennon62@gmail.com>
2024-12-03 23:49:41 +0000

    Day 2 Complete

--- Day2/solution2.py
+++ Day2/solution2.py
@@ -1,8 +1,8 @@
 import os
 print(os.getcwd())
 #Change this line to your directory for Day 1 
-#os.chdir("C:/Users/Peter/Desktop/Software Projects/Python/AdventOfCode/Day1")
-os.chdir("C:/Users/peter/OneDrive/Documents/Programming/Python/AdventOfCode2024/Day2")
+os.chdir("C:/Users/Peter/Desktop/Software Projects/Python/AdventOfCode/Day2")
+#os.chdir("C:/Users/peter/OneDrive/Documents/Programming/Python/AdventOfCode2024/Day2")
 
 def readFile(name):
     fileLocation = name + ".txt"
@@ -12,41 +12,63 @@ def readFile(name):
 
     #Reads each line of the text file, removing the spaces and covert into ints
     for line in f:
-        temp = line.replace(" ", "")
-        #reportLevels.append([int(item) for item in temp])
-        values = []
-        for i in range(len(temp)):
-            if temp[i].isdigit() == True:
-                values.append(int(temp[i]))
-        reportLevels.append(values)
+        temp = [int(entry) for entry in line.split() if entry.isdigit()]
+        reportLevels.append(temp)
 
     reportLevels.pop(0)
     return reportLevels
 
 def checkLevels(reportLine):
     safeLevels = True
+    firstPass = True
     lastdiff = 0
     currentdiff = 0
     i = 0
-
+    
     #for i in range(len(reportLine) - 1):
-    while safeLevels == True and i < len(reportLine):
+    while safeLevels == True and i < len(reportLine) - 1:
         currentdiff = reportLine[i] - reportLine[i+1]
 
-        if abs(currentdiff) <= 3 and abs(currentdiff) > 0:
-            if currentdiff > 0 and lastdiff > 0:
-                print("We are increasing")
-            elif currentdiff < 0 and lastdiff < 0:
-                print("We are decreasing")
+        if firstPass == False:
+            if abs(currentdiff) <= 3 and abs(currentdiff) > 0:
+                if currentdiff > 0 and lastdiff > 0:
+                    #print("We are increasing")
+                    safeLevels = True
+                elif currentdiff < 0 and lastdiff < 0:
+                    #print("We are decreasing")
+                    safeLevels = True
+                else:
+                    #print("We are hectic!")
+                    safeLevels = False
             else:
-                print("We are hectic!")
+                #print("We are mental!")
                 safeLevels = False
+        else:
+            if abs(currentdiff) <= 3 and abs(currentdiff) > 0:
+                #print("We are Safe")  
+                safeLevels = True             
+            else:
+                #print("We are dangerous!")
+                safeLevels = False
+            firstPass = False
         
+        lastdiff = currentdiff
         i += 1
 
-        print(reportLine[i])
+    return safeLevels
 
-report  = readFile("TestInput")
-checkLevels(report[0])
+report  = readFile("input")
+print(report[0])
 
+count = 0
+for entries in report:
+    for i in range(len(entries)):
+        temp = entries[:]
+        temp.pop(i)
+        if checkLevels(temp) == True:
+            count += 1
+            break
+
+print("Number of safe reports:")
+print(count)
 
