06de6e4ecbc0452df9703b29a35d4177e6d98998
Peter Sousa Lennon <psousalennon01@qub.ac.uk>
2024-12-04 22:25:00 +0000

    More of Day 3

--- Day3/solution3.py
+++ Day3/solution3.py
@@ -2,8 +2,8 @@ import os
 import re as r
 print(os.getcwd())
 #Change this line to your directory for Day 1 
-os.chdir("C:/Users/Peter/Desktop/Software Projects/Python/AdventOfCode/Day2") #Desktop
-#os.chdir("C:/Users/peter/OneDrive/Documents/Programming/Python/AdventOfCode2024/Day2") #Laptop
+#os.chdir("C:/Users/Peter/Desktop/Software Projects/Python/AdventOfCode/Day2") #Desktop
+os.chdir("C:/Users/peter/OneDrive/Documents/Programming/Python/AdventOfCode2024/Day3") #Laptop
 
 def readFile(name):
     fileLocation = name + ".txt"
@@ -14,10 +14,18 @@ def readFile(name):
 
 
 def findMul(text):
-    hits = r.findall("mul\([0-9]{1,3},[0-9]{1,3}\)|do\(\)|don\'t\(\)", text)
+    hits = r.findall(r"mul\([0-9]{1,3},[0-9]{1,3}\)|do\(\)|don\'t\(\)", text)
+    return hits
 
-    print(hits)
+def calcPart1(mul):
+    for op in mul:
+        if op[0] == "m":
+            
+
+
 
 filename = "input"
-
+problem = readFile(filename)
+mulList = findMul(problem)
+calcPart1(mulList)
 
